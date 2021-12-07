ff0d78b2f3c9348bc4a52a9d80e3ac92e1385e67
Rob Ryan <rob@affclicks.com>
2021-12-07 21:28:19 +1100

    Day 7

---
+++ days/7-1/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "fmt"
+    //"sort"
+    "adventofcode/2021/modules/readinput"
+)
+
+func main() {
+    positions := readinput.ReadInts("inputs/7/input.txt", ",")
+
+    // could also potentially just use the median value
+    // sort.Ints(positions)
+    // fmt.Println(positions[int(len(positions)/2)])
+
+    var max_position int
+    for i:=0; i<len(positions); i++ {
+        if(positions[i] > max_position) {
+            max_position = positions[i]
+        }
+    }
+    
+    var best_run int
+    var best_run_position int
+    var current_run int
+
+    for i:=0; i<max_position; i++ {
+        current_run = 0
+        for j:=0; j<len(positions); j++ {
+            if positions[j] > i {
+                current_run += positions[j] - i
+            } else {
+                current_run += i - positions[j]
+            }
+        }
+
+        if best_run == 0 || current_run < best_run {
+            best_run = current_run
+            best_run_position = i
+        }
+    }
+
+    fmt.Println(best_run)
+    fmt.Println(best_run_position)
+}
---
+++ days/7-2/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+    "fmt"
+    "adventofcode/2021/modules/readinput"
+)
+
+func main() {
+    positions := readinput.ReadInts("inputs/7/input.txt", ",")
+
+    var max_position int
+    for i:=0; i<len(positions); i++ {
+        if(positions[i] > max_position) {
+            max_position = positions[i]
+        }
+    }
+    
+    var best_run int
+    var best_run_position int
+    var current_run int
+
+    for i:=0; i<max_position; i++ {
+        current_run = 0
+        for j:=0; j<len(positions); j++ {
+            var gap int
+
+            if positions[j] > i {
+                gap = positions[j] - i
+            } else {
+                gap = i - positions[j]
+            }
+
+            var gap_total int
+            for k:=0; k<gap; k++ {
+                gap_total += k + 1
+            }
+
+            // triangle numbers
+            current_run += int((gap * (gap + 1)) / 2)
+        }
+
+        if best_run == 0 || current_run < best_run {
+            best_run = current_run
+            best_run_position = i
+        }
+    }
+
+    fmt.Println(best_run)
+    fmt.Println(best_run_position)
+}
