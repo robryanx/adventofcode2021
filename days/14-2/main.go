80fbe5153d183d0a56818f77e9cd3be1a72e2952
Rob Ryan <rob@affclicks.com>
2021-12-16 22:19:38 +1100

    Day 14: Step 2 list version

--- days/14-2/main.go
+++ days/14-2/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	//"fmt"
 	"adventofcode/2021/modules/readinput"
 	"fmt"
 	"strings"
 )
+
+type pattern struct {
+    chars string
+    count int
+}
 
 func main() {
     lines := readinput.ReadStrings("inputs/14/input.txt", "\n")
@@ -19,9 +23,82 @@ func main() {
         replacements[line_parts[0]] = line_parts[2][0]
     }
 
+    frequency := list_version(replacements, pattern, 40)
+
+    smallest_count := -1
+    largest_count := 0
+
+    for _, count := range frequency {
+        if smallest_count == -1 || smallest_count > count {
+            smallest_count = count
+        }
+
+        if largest_count < count {
+            largest_count = count
+        }
+    }
+
+    fmt.Println(largest_count - smallest_count)
+}
+
+func add_pattern(pattern_list []*pattern, chars string, count int) []*pattern {
+    for i:=0; i<len(pattern_list); i++ {
+        if pattern_list[i].chars == chars {
+            pattern_list[i].count += count
+
+            return pattern_list
+        }
+    }
+
+    pattern_list = append(pattern_list, &pattern{chars: chars, count: count})
+
+    return pattern_list
+}
+
+func list_version(replacements map[string]byte, start_list []byte, steps int) map[string]int {
+    var pattern_list []*pattern
+
+    var check string
+    for i:=0; i<len(start_list)-1; i++ {
+        check = string([]byte{start_list[i], start_list[i+1]})
+
+        pattern_list = append(pattern_list, &pattern{chars: check, count: 1})
+    }
+    
+    for step:=0; step<steps; step++ {
+        new_patterns := []*pattern{}
+
+        for _, pattern_i := range pattern_list {
+            if _, ok := replacements[pattern_i.chars]; ok {
+                check = string([]byte{pattern_i.chars[0], replacements[pattern_i.chars]})
+
+                new_patterns = add_pattern(new_patterns, check, pattern_i.count)
+
+                check = string([]byte{replacements[pattern_i.chars], pattern_i.chars[1]})
+
+                new_patterns = add_pattern(new_patterns, check, pattern_i.count)
+            } else {
+                new_patterns = add_pattern(new_patterns, pattern_i.chars, pattern_i.count)
+            }
+        }
+
+        pattern_list = new_patterns
+    }
+
+    frequency := make(map[string]int)
+    for i:=0; i<len(pattern_list); i++ {
+        frequency[string(pattern_list[i].chars[0])] += pattern_list[i].count
+    }
+
+    frequency[string(start_list[len(start_list)-1])]++
+
+    return frequency
+}
+
+func concat_version(replacements map[string]byte, pattern []byte, steps int) map[string]int {
     var build_pos int
     var check string
-    for step:=0; step<40; step++ {
+    for step:=0; step<steps; step++ {
         fmt.Printf("Step %d\n", (step+1));
 
         build_pos = 0
@@ -61,20 +138,5 @@ func main() {
         frequency[string(pattern[i])]++
     }
 
-    fmt.Println(frequency)
-
-    smallest_count := -1
-    largest_count := 0
-
-    for _, count := range frequency {
-        if smallest_count == -1 || smallest_count > count {
-            smallest_count = count
-        }
-
-        if largest_count < count {
-            largest_count = count
-        }
-    }
-
-    fmt.Println(largest_count - smallest_count)
+    return frequency
 }
